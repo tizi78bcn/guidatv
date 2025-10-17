30580c76672e77dfd5026c0403fb8e7a610c0878
tizi78bcn <tiziano.branchi.bcn@gmail.com>
2025-10-17 14:31:47 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -47,5 +47,8 @@ matches = [
 def get_matches():
     return jsonify(matches)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
